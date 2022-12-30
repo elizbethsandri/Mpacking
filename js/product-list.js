4b840a2b9e482723d522565cc5b705d0c97d29c8
Elizabeth Sandri <elizabethsandri18@gmail.com>
2022-12-30 02:43:12 +0000

    Made the about and business section responsive

--- js/product-list.js
+++ js/product-list.js
@@ -1,1 +1,10 @@
-const bootstrap = require('bootstrap');
+
+
+var $grid = $('.collection-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', 'a', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
